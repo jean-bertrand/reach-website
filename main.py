a72ca713c2fb6e387f9b5240e059907d51bb47d5
Jean-Bertrand Uwilingiyimana <jeanbertrand@Jean-Bertrands-MacBook-Pro.local>
2018-08-03 13:52:50 -0400

    - disable flask-scss - add routes for toc and privacy policy

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
-from flask_scss import Scss
+#from flask_scss import Scss
 
 app = Flask(__name__, static_url_path='/static')
 
 # SCSS Configuration
-Scss(app, static_dir='static', asset_dir='assets')
+#Scss(app, static_dir='static', asset_dir='assets')
 
 
 # Routes
 @app.route('/')
 def main():
-    return render_template('/index.html')
+    return render_template('/home.html')
+
+@app.route('/terms-of-use')
+def toc():
+    return render_template('/terms-of-use.html')
+
+@app.route('/privacy-policy')
+def privacy_policy():
+    return render_template('/privacy-policy')
 
 if __name__ == '__main__':
     app.run(debug=True)
