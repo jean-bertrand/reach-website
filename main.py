1d1a08be857460a6a435e7f2cf019253b0b15128
Jean-Bertrand Uwilingiyimana <jeanbertrand@Jean-Bertrands-MacBook-Pro.local>
2018-08-03 12:55:43 -0400

    define path for static assets

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # SCSS Configuration
 Scss(app, static_dir='static', asset_dir='assets')
